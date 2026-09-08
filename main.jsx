b0c9ceea2cb60351b8a114def692b627e9ff24d3
raja36-u <djindiaremix@gmail.com>
2026-09-09 01:15:50 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -7,6 +7,10 @@ import {
 } from "lucide-react";
 import "./styles.css";
 import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+);
 
 const ANIME = [
   { id: 1, title: "Solo Leveling", episode: "Episode 12", views: "12.8K", likes: 1200, date: "12/04/2026", pinned: true, category: "Top Rated",
